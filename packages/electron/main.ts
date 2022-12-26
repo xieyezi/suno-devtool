65b2489cd8b42f4582025b14078802cc91396416
xieyezi <1435398529@qq.com>
2022-12-26 17:43:06 +0800

    feat: load chorme dev tool success

--- packages/electron/main.ts
+++ packages/electron/main.ts
@@ -6,19 +6,27 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    titleBarStyle: "hidden",
+    //titleBarStyle: "hidden",
     //frame: false
     //webPreferences: {
     //  preload: path.join(__dirname, './preload.js'),
     //},
   });
 
-  if (process.env.VITE_DEV_SERVER_URL) {
-    win.loadURL(process.env.VITE_DEV_SERVER_URL);
-  } else {
-    win.loadFile(path.join(__dirname, "../../core/dist/index.html"));
-  }
-  win.webContents.openDevTools({ mode: "right" });
+  //if (process.env.VITE_DEV_SERVER_URL) {
+  //  win.loadURL(process.env.VITE_DEV_SERVER_URL);
+  //} else {
+  //  win.loadFile(path.join(__dirname, "../../core/dist/index.html"));
+  //}
+  win.loadFile(
+    path.join(__dirname, "../../core/dev-tool/front_end/devtools_app.html"),
+    {
+      query: {
+        ws: "localhost:9222/devtools/page/124655E7CF111310BAA700479191BCBC",
+      },
+    }
+  );
+  //win.webContents.openDevTools({ mode: "right" });
 }
 
 app.whenReady().then(() => {
